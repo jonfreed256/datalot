c9e13e4703c51b726cb0ab84fe90f362be06619d
github username <github email address>
2019-05-10 16:13:43 -0400

    Updates

---
+++ precache-manifest.bb0290a10a34e03c690ad66b241083d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8cb19d9033069f523d721c9acd40a0f",
+    "url": "/datalot/index.html"
+  },
+  {
+    "revision": "30c00dfbe92e8d791c49",
+    "url": "/datalot/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "90758ac1f499d191bb92",
+    "url": "/datalot/static/css/main.7e45d3cd.chunk.css"
+  },
+  {
+    "revision": "30c00dfbe92e8d791c49",
+    "url": "/datalot/static/js/2.41e73633.chunk.js"
+  },
+  {
+    "revision": "90758ac1f499d191bb92",
+    "url": "/datalot/static/js/main.c9bbc007.chunk.js"
+  },
+  {
+    "revision": "c6a4dc3071c18b755d7b",
+    "url": "/datalot/static/js/runtime~main.6cab0514.js"
+  },
+  {
+    "revision": "ae4e98e5b6029ea85ec2e2d2cdd98d59",
+    "url": "/datalot/static/media/bg.ae4e98e5.jpg"
+  },
+  {
+    "revision": "b7a43e5b03aa9f32a02dfb6d521c2523",
+    "url": "/datalot/static/media/cars.b7a43e5b.jpg"
+  }
+]);
